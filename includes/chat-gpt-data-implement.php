e4da37f41b90f744d59f6c5f14bdf2df3f6d8b82
Prakash Tyata <prakashtyata@gmail.com>
2023-02-03 16:39:54 +0545

    removed the temp_html

--- includes/chat-gpt-data-implement.php
+++ includes/chat-gpt-data-implement.php
@@ -19,42 +19,60 @@ function pra_woocommerce_after_single_product_summary() {
     $devlink = get_post_meta( get_the_ID(), 'product_faq_textarea', true );
     $dev_faq = json_decode($devlink);
 
-    if (!empty($devlink)) {
-        $html = '<div class="dev_rank-math-list ">';
-        $html .= '<h3>FAQs</h3>';
-        $html .= '<div class="rank-math-list ">';
-        foreach($dev_faq as $arrays) {
-            $html .='<div class="rank-math-list-item">';
-                if (!empty($arrays->question)){ $html .='<h5 class="rank-math-question show">'.$arrays->question.'</h5>'; }
-                else{ $html .='<h5 class="rank-math-question show">'.$arrays->Question.'</h5>'; }
+    if (!empty($devlink)) { ?>
 
-            $html .='<div class="rank-math-answer showing">';
-                if (!empty($arrays->answer)){ $html .='<p>'.$arrays->answer.'</p>'; }
-                else{ $html .='<p>'.$arrays->Answer.'</p>'; }
+        <div class="dev_rank-math-list">
+            <h3>FAQs</h3>
+            <div class="rank-math-list ">
+                <?php
+                    foreach($dev_faq as $arrays) { ?>
+                        <div class="rank-math-list-item">
+                            <?php 
+                                if (!empty($arrays->question)){ ?>
+                                    <h5 class="rank-math-question show"> <?php echo $arrays->question; ?></h5>
+                                <?php }
+                                else if (!empty($arrays->Question)){ ?>
+                                    <h5 class="rank-math-question show"><?php echo $arrays->Question; ?></h5>'; 
+                                <?php }
+                                else{ ?>
+                                    <h5 class="rank-math-question show"><?php echo $arrays->FAQ; ?></h5>
+                                <?php } ?>
+                                <div class="rank-math-answer showing">
+                                    <?php
+                                    if (!empty($arrays->answer)){ ?>
+                                        <p><?php echo $arrays->answer; ?></p> 
+                                    <?php }
+                                    else if (!empty($arrays->Answer)){ ?>
+                                        <p><?php $arrays->Answer; ?></p> 
+                                    <?php }
+                                    else{ ?>
+                                        <p><?php echo $arrays->Answer; ?></p> 
+                                    <?php } ?>
+                                </div>
+                            ?>
+                        </div>
+                    <?php }
+                ?>
 
-            $html .='</div>';
-            $html .='</div>';
-        }
-        $html .= '</div>';
-        $html .= '</div>';
-        echo $html; 
-
-        echo '<script>
-                jQuery(document).ready(function(){
-                  jQuery(".rank-math-list-item .rank-math-question").click(function(){
+            </div>
+        </div>
+        <script>
+            jQuery(document).ready(function(){
+                jQuery(".rank-math-list-item .rank-math-question").click(function(){
                     jQuery(this).parents(".rank-math-list-item").siblings().find(".rank-math-answer").hide(600);
-                      jQuery(this).parents(".rank-math-list-item").siblings().find(".rank-math-question").removeClass("open");
-                    if(jQuery(this).next().is(":visible")){
-                      jQuery(this).next().hide(600);
-                      jQuery(this).removeClass("open");
-                    }
-                    else{
-                      jQuery(this).addClass("open");
-                      jQuery(this).next().show(600);
-                    }
-                  });
-                });         
-                </script>';
+                    jQuery(this).parents(".rank-math-list-item").siblings().find(".rank-math-question").removeClass("open");
+                        if(jQuery(this).next().is(":visible")){
+                            jQuery(this).next().hide(600);
+                            jQuery(this).removeClass("open");
+                        }
+                        else{
+                            jQuery(this).addClass("open");
+                            jQuery(this).next().show(600);
+                        }
+                });
+            });         
+        </script>
+    <?php
     }
 
 } 
@@ -67,16 +85,14 @@ function hook_javascript() {
     $dev_faq = json_decode($devlink);
 
     if (!empty($dev_faq)) {
-        $counter = 0;
+        $counter = 0; 
         $temp_html ='';
         $temp_html .='<script type="application/ld+json">
             {
               "@context": "https://schema.org",
               "@type": "FAQPage",
               "mainEntity": ['; 
-
              foreach($dev_faq as $arrays) { 
-
                 if (!empty($arrays->question)){
                     $temp_html .= '{
                         "@type": "Question",
@@ -105,11 +121,9 @@ function hook_javascript() {
                 }
                 $counter++;
             }   
-
         $temp_html .=' ]
             }
             </script>'; 
-
         echo $temp_html;
     }
 }
@@ -122,9 +136,9 @@ add_action('wp_head', 'hook_javascript');
 function dev_meta_description() {
     if (is_product()){
         global $post;
-        $seo_title = get_post_meta( $post->ID, 'product_seo_description', true );
-        echo '<meta name="description" content="' . $seo_title . '" />' . "\n";
-    }
+        $seo_title = get_post_meta( $post->ID, 'product_seo_description', true ); ?>
+        <meta name="description" content="<?php echo $seo_title ?>" /> <br>
+    <?php }
 
 }
 add_action( 'wp_head', 'dev_meta_description');
